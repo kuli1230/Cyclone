d3875abf9fb375a345d17b8ad57dfa74556a55f9
JackWhite20 <phil.westf@web.de>
2015-11-24 17:47:57 +0100

    Added javadoc to the new classes.

--- CycloneCore/src/main/java/de/jackwhite20/cyclone/db/Function.java
+++ CycloneCore/src/main/java/de/jackwhite20/cyclone/db/Function.java
@@ -2,6 +2,8 @@ package de.jackwhite20.cyclone.db;
 
 /**
  * Created by JackWhite20 on 23.11.2015.
+ *
+ * Represents an aggregate function with the type, column and alternatively an AS statement.
  */
 public class Function {
 
@@ -11,6 +13,13 @@ public class Function {
 
     private String as;
 
+    /**
+     * Creates a new aggregate function with the given type, column and AS statement.
+     *
+     * @param type the type.
+     * @param column the column to perform the function on.
+     * @param as the AS statement.
+     */
     public Function(Type type, String column, String as) {
 
         this.type = type;
@@ -18,26 +27,51 @@ public class Function {
         this.as = as;
     }
 
+    /**
+     * Creates a new aggregate function with the given type, column and no AS statement.
+     *
+     * @param type the type.
+     * @param column the column to perform the function on.
+     */
     public Function(Type type, String column) {
 
         this(type, column, null);
     }
 
+    /**
+     * Gets the type.
+     *
+     * @return the type.
+     */
     public Type type() {
 
         return type;
     }
 
+    /**
+     * Gets the column.
+     *
+     * @return the column to perform the function on.
+     */
     public String column() {
 
         return column;
     }
 
+    /**
+     * Gets the AS statement.
+     * Is null when no AS statement has been given.
+     *
+     * @return the AS statement.
+     */
     public String as() {
 
         return as;
     }
 
+    /**
+     * Represents a aggregate function.
+     */
     public enum Type {
 
         SUM("SUM"), AVG("AVG"), MIN("MIN"), MAX("MAX"), COUNT("COUNT");
@@ -49,6 +83,11 @@ public class Function {
             this.sql = sql;
         }
 
+        /**
+         * Gets the SQL name from the type.
+         *
+         * @return the sql name.
+         */
         public String sql() {
 
             return sql;
--- CycloneCore/src/main/java/de/jackwhite20/cyclone/db/Join.java
+++ CycloneCore/src/main/java/de/jackwhite20/cyclone/db/Join.java
@@ -2,6 +2,8 @@ package de.jackwhite20.cyclone.db;
 
 /**
  * Created by JackWhite20 on 13.11.2015.
+ *
+ * Represents a SQL JOIN statement with the table to join on, the column and a value or other column.
  */
 public class Join {
 
@@ -11,6 +13,13 @@ public class Join {
 
     private String onValue;
 
+    /**
+     * Creates a new SQL JOIN statement with the given values.
+     *
+     * @param joinTable the table.
+     * @param onColumn the column.
+     * @param onValue the value.
+     */
     public Join(String joinTable, String onColumn, String onValue) {
 
         this.joinTable = joinTable;
@@ -18,16 +27,31 @@ public class Join {
         this.onValue = onValue;
     }
 
+    /**
+     * Gets the table.
+     *
+     * @return the table.
+     */
     public String joinTable() {
 
         return joinTable;
     }
 
+    /**
+     * Gets the column.
+     *
+     * @return the column.
+     */
     public String onColumn() {
 
         return onColumn;
     }
 
+    /**
+     * Gets the value or other column.
+     *
+     * @return the value/column.
+     */
     public String onValue() {
 
         return onValue;
